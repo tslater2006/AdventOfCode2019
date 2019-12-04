37e9e2988551c82592533361d941cc6c236b0699
tslater2006 <tslater2006@gmail.com>
2019-12-03 23:44:07 -0600

    Day 4 Solution

--- Solutions/Year2019/Day04/Solution.cs
+++ Solutions/Year2019/Day04/Solution.cs
@@ -1,17 +1,103 @@
+using System;
+using System.Linq;
+
 namespace AdventOfCode.Solutions.Year2019 {
 
     class Day04 : ASolution {
         
-        public Day04() : base(4, 2019, "") {
+        public Day04() : base(4, 2019, "Secure Container") {
             
         }
 
         protected override string SolvePartOne() {
-            return null; 
+            int[] range = Input[0].Split('-').Select(s => int.Parse(s)).ToArray();
+
+            return Enumerable.Range(range[0], range[1] - range[0]).Where(i => ValidPassword(i)).Count().ToString();
         }
 
         protected override string SolvePartTwo() {
-            return null; 
+            int[] range = Input[0].Split('-').Select(s => int.Parse(s)).ToArray();
+
+            return Enumerable.Range(range[0], range[1] - range[0]).Where(i => ValidPassword2(i)).Count().ToString();
         }
+
+        protected bool ValidPassword(int n)
+        {
+            int temp = n;
+
+            /* check for digit ascending */
+            bool isAscending = true;
+            bool hasDuplicate = false;
+            var lastDigit = 0;
+            while (temp != 0)
+            {
+                lastDigit = temp % 10;
+                temp = temp / 10;
+
+                if (temp % 10 > lastDigit)
+                {
+                    isAscending = false;
+                    break;
+                }
+
+                if (temp % 10 == lastDigit)
+                {
+                    hasDuplicate = true;
+                }
+            }
+            return isAscending && hasDuplicate;
+        }
+
+        protected bool ValidPassword2(int n)
+        {
+            int temp = n;
+
+            /* check for digit ascending */
+            bool isAscending = true;
+            bool hasDuplicate = false;
+            bool checkingForDups = true;
+
+            int duplicateCount = 0;
+
+            var lastDigit = 0;
+            while (temp != 0)
+            {
+                lastDigit = temp % 10;
+                temp = temp / 10;
+
+                if (temp % 10 > lastDigit)
+                {
+                    isAscending = false;
+                    break;
+                }
+
+                if (temp % 10 == lastDigit && checkingForDups)
+                {
+                    if (hasDuplicate)
+                    {
+                        duplicateCount++;
+                    }
+                    else
+                    {
+                        hasDuplicate = true;
+                        duplicateCount = 2;
+                    }
+                } else
+                {
+                    if ( duplicateCount == 2)
+                    {
+                        checkingForDups = false;
+                    } else
+                    {
+                        duplicateCount = 0;
+                        hasDuplicate = false;
+                    }
+                }
+
+
+            }
+            return isAscending && hasDuplicate;
+        }
+
     }
 }
