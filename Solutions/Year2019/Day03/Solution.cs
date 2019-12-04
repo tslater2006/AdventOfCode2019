61ce94b9eed8ad680f6bd17bc7bfb918c0e3fe53
tslater2006 <tslater2006@gmail.com>
2019-12-03 23:43:39 -0600

    Improve Day 3 Performance

--- Solutions/Year2019/Day03/Solution.cs
+++ Solutions/Year2019/Day03/Solution.cs
@@ -6,23 +6,23 @@ using System.Linq;
 namespace AdventOfCode.Solutions.Year2019 {
 
     class Day03 : ASolution {
-        List<Point> Wire1 = null;
-        List<Point> Wire2 = null;
+        Dictionary<Point,int> Wire1 = null;
+        Dictionary<Point,int> Wire2 = null;
+
         public Day03() : base(3, 2019, "Crossed Wires")
         {
             Wire1 = ParseWire(Input[0]);
             Wire2 = ParseWire(Input[1]);
         }
 
-        internal List<Point> ParseWire (string path) {
+        internal Dictionary<Point,int> ParseWire (string path) {
             var directions = path.Split(',');
-            List<Point> newWire = new List<Point>();
-
-            newWire.Add(new Point(0, 0));
-
+            Dictionary<Point, int> newWire = new Dictionary<Point, int>();
+            int stepCount = 0;
+            Point currentPoint = new Point(0, 0);
+            
             foreach (var direction in directions)
             {
-                var currentPoint = newWire.Last();
                 var amount = int.Parse(direction.Substring(1));
                 int xOffset = 0;
                 int yOffset = 0;
@@ -46,7 +46,10 @@ namespace AdventOfCode.Solutions.Year2019 {
                 for (var x = 0; x < amount; x++)
                 {
                     currentPoint = new Point(currentPoint.X + xOffset, currentPoint.Y + yOffset);
-                    newWire.Add(currentPoint);
+                    if (newWire.ContainsKey(currentPoint) == false)
+                    {
+                        newWire.Add(currentPoint, stepCount++);
+                    }
                 }
             }
 
@@ -55,7 +58,7 @@ namespace AdventOfCode.Solutions.Year2019 {
 
         protected override string SolvePartOne() {
 
-            var intersects = Wire1.Skip(1).Intersect(Wire2.Skip(1));
+            var intersects = Wire1.Keys.Intersect(Wire2.Keys);
             
             var closest = intersects.OrderBy(p => Math.Abs(p.X) + Math.Abs(p.Y)).First();
             
@@ -66,9 +69,8 @@ namespace AdventOfCode.Solutions.Year2019 {
 
         protected override string SolvePartTwo() {
 
-            var intersects = Wire1.Skip(1).Intersect(Wire2.Skip(1));
-
-            var closestWalk = intersects.Select(p => Wire1.IndexOf(p) + Wire2.IndexOf(p)).OrderBy(i => i).First();
+            var intersects = Wire1.Keys.Intersect(Wire2.Keys);
+            var closestWalk = intersects.Select(p => Wire1[p] + Wire2[p]).OrderBy(i => i).First();
 
             return closestWalk.ToString(); 
         }
