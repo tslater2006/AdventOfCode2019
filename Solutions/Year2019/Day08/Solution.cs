cb91878c634ab2345022ebe62dddba39addaf078
tslater2006 <tslater2006@gmail.com>
2019-12-08 05:30:10 -0600

    Day 8 solution

--- Solutions/Year2019/Day08/Solution.cs
+++ Solutions/Year2019/Day08/Solution.cs
@@ -1,17 +1,35 @@
+using AdventOfCode.Utilities;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+
 namespace AdventOfCode.Solutions.Year2019 {
 
     class Day08 : ASolution {
-        
-        public Day08() : base(8, 2019, "") {
-            
+        ElfImage image;
+        public Day08() : base(8, 2019, "Space Image Format") {
+            int[] data = Input[0].ToCharArray().Select(c => int.Parse(c.ToString())).ToArray() ;
+
+            image = new ElfImage(data, 25, 6);
         }
 
         protected override string SolvePartOne() {
-            return null; 
+
+            var minLayer = image.Layers.OrderBy(l => l.Cast<int>().Count(p => p == 0)).First();
+
+            var oneDigits = minLayer.Cast<int>().Count(p => p == 1);
+            var twoDigits = minLayer.Cast<int>().Count(p => p == 2);
+
+            var ans = oneDigits * twoDigits;
+
+            return ans.ToString(); 
         }
 
         protected override string SolvePartTwo() {
-            return null; 
+            var bmp = image.RenderImage();
+            bmp.Save("Day8Part2.png", ImageFormat.Png);
+
+            return "Answer saved as Day8Part2.png"; 
         }
     }
 }
---
+++ Utilities/ElfImage.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace AdventOfCode.Utilities
+{
+    class ElfImage
+    {
+        public List<int[,]> Layers = new List<int[,]>();
+        public int Width;
+        public int Height;
+
+        public ElfImage(int[] imageData, int width, int height)
+        {
+            Width = width;
+            Height = height;
+            int layerSize = width * height;
+            int[,] curLayer = null;
+            for (var x = 0; x < imageData.Length; x++)
+            {
+                if (x % layerSize == 0)
+                {
+                    /* time for a new layer array */
+                    curLayer = new int[height, width];
+                    Layers.Add(curLayer);
+                }
+
+                var layerIndex = x % layerSize;
+
+                var row = layerIndex / width;
+                var col = layerIndex % width;
+
+                curLayer[row, col] = imageData[x];
+
+            }
+        }
+
+        public int[,] FlattenLayers()
+        {
+            int[,] final = new int[Height, Width];
+
+            for (var x = 0; x < Width; x++)
+            {
+                for (var y = 0; y < Height; y++)
+                {
+                    for (var z = 0; z < Layers.Count; z++)
+                    {
+                        if (Layers[z][y, x] != 2)
+                        {
+                            final[y, x] = Layers[z][y, x];
+                            break;
+                        }
+                    }
+                }
+            }
+
+            return final;
+        }
+
+        public Bitmap RenderImage()
+        {
+            int[,] finalImage = FlattenLayers();
+
+            Bitmap bmp = new Bitmap(Width, Height);
+
+            for (var x = 0; x < Width; x++)
+            {
+                for (var y = 0; y < Height; y++)
+                {
+                    bmp.SetPixel(x, y, finalImage[y, x] == 0 ? Color.Black : Color.White);
+                }
+            }
+
+            return bmp;
+        }
+
+    }
+
+}
