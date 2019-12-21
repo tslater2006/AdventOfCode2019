98615ca651d1067e088ae93f05b986bbb9dcf506
tslater2006 <tslater2006@gmail.com>
2019-12-21 00:13:41 -0600

    Day 21 Solution.

--- Solutions/Year2019/Day21/Solution.cs
+++ Solutions/Year2019/Day21/Solution.cs
@@ -1,17 +1,50 @@
+using AdventOfCode.Utilities;
+using System;
+using System.Linq;
+
 namespace AdventOfCode.Solutions.Year2019 {
 
     class Day21 : ASolution {
-        
+
+        IntCodeVM vm = null;
         public Day21() : base(21, 2019, "") {
             
         }
 
         protected override string SolvePartOne() {
-            return null; 
+
+            vm = new IntCodeVM(Input[0]);
+
+            var springScript = "NOT A T\nOR T J\nNOT B T\nOR T J\nNOT C T\nOR T J\nAND D J\nWALK\n";
+
+            vm.WriteInputString(springScript);
+
+            var haltType = vm.RunProgram();
+
+            var ans = vm.ReadOutputs().Last();
+
+            return ans.ToString(); 
         }
 
         protected override string SolvePartTwo() {
-            return null; 
+
+            vm = new IntCodeVM(Input[0]);
+
+            var springScript = "OR A J\nAND B J\nAND C J\nNOT J J\nAND D J\nOR E T\nOR H T\nAND T J\nRUN\n";
+
+            vm.WriteInputString(springScript);
+
+            var haltType = vm.RunProgram();
+
+            foreach(var l in vm.ReadOutputs())
+            {
+                Console.Write((char)l);
+            }
+
+            var ans = vm.ReadOutputs().Last();
+
+            return ans.ToString();
+
         }
     }
 }
