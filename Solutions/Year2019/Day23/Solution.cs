c6f3e11d44051b34d6167689c30d98d5d0541b48
tslater2006 <tslater2006@gmail.com>
2019-12-22 23:46:36 -0600

    Add puzzle title to Day 23

--- Solutions/Year2019/Day23/Solution.cs
+++ Solutions/Year2019/Day23/Solution.cs
@@ -6,7 +6,7 @@ namespace AdventOfCode.Solutions.Year2019 {
 
     class Day23 : ASolution {
 
-        public Day23() : base(23, 2019, "") {
+        public Day23() : base(23, 2019, "Category Six") {
             
         }
 
